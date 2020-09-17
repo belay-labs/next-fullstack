51f58a5079535eb99bffdd3209e84381cbbc9f88
cathykc <cathykaichen@gmail.com>
2020-09-17 14:27:34 -0600

    message

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,18 @@ export default function Home() {
   return (
     <div className="rounded shadow-lg bg-white px-6 py-4">
       <pre>next-fullstack</pre>
-      <div></div>
+      <hr className="my-4" />
+      <p>
+        This is a hosted demo of a full stack boilerplate with Next.js. See the{" "}
+        <a
+          className="text-yellow-600 hover:text-yellow-700"
+          href="https://github.com/belay-labs/next-fullstack"
+          target="_blank"
+        >
+          README
+        </a>{" "}
+        for more information.
+      </p>
     </div>
   );
 }
